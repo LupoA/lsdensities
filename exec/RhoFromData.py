e59986d3be0da62a9d774d3d57151b6eac3f8c23
Alessandro Lupo <Alessandro.Lupo@ed.ac.uk>
2023-05-11 14:46:50 +0100

    typo

--- exec/RhoFromData.py
+++ exec/RhoFromData.py
@@ -4,7 +4,8 @@ from importall import *
 
 eNorm = False
 
-    # NOT YET TESTED
+# NOT YET TESTED
+
 
 def init_variables(args_):
     in_ = u.inputs()
@@ -23,6 +24,7 @@ def init_variables(args_):
     in_.plots = args_.plots
     return in_
 
+
 def main():
     print(LogMessage(), "Initialising")
     args = parseArgumentRhoFromData()
@@ -35,7 +37,7 @@ def main():
 
     #   Reading datafile, storing correlator
     rawcorr, par.time_extent, par.num_samples = u.read_datafile(par.datapath)
-    par.mpi = 1.
+    par.mpi = 1.0
     par.assign_values()
     par.report()
 
@@ -49,16 +51,16 @@ def main():
     corr.evaluate()
     print(LogMessage(), "Evaluate covariance")
     corr.evaluate_covmatrix(plot=False)
-    #corr.eval_corrmatrix(plot=True)
+    # corr.eval_corrmatrix(plot=True)
 
-    tmax = 56   #   soon to be parsed from command line it is the latest correlator we use
+    tmax = 56  #   soon to be parsed from command line it is the latest correlator we use
     par.tmax = tmax
 
     #   make it into a mp sample
     mpcorr_sample = mp.matrix(par.num_boot, tmax)
     for n in range(par.num_boot):
         for i in range(tmax):
-            mpcorr_sample[n, i] = mpf(str(corr.sample[n][i+1]))
+            mpcorr_sample[n, i] = mpf(str(corr.sample[n][i + 1]))
     #   Get cov for B matrix
     mpcov = mp.matrix(tmax)
     for i in range(tmax):
@@ -73,13 +75,23 @@ def main():
     print(LogMessage(), "S/S - 1 ::: ", diff)
 
     #   Get rho
-    rho = mp.matrix(par.Ne,1)
+    rho = mp.matrix(par.Ne, 1)
     drho = mp.matrix(par.Ne, 1)
     for e_i in range(par.Ne):
         estar = espace_mp[e_i]
         #   get lstar
-        lstar_fp = getLstar_Eslice(estar, S, mpcov, cNorm, par, eNorm_=False, lambda_min=0.01, lambda_max=0.6, num_lambda=20)
-        scale_fp = lstar_fp / (1-lstar_fp)
+        lstar_fp = getLstar_Eslice(
+            estar,
+            S,
+            mpcov,
+            cNorm,
+            par,
+            eNorm_=False,
+            lambda_min=0.01,
+            lambda_max=0.6,
+            num_lambda=20,
+        )
+        scale_fp = lstar_fp / (1 - lstar_fp)
         scale_mp = mpf(scale_fp)
         a0 = A0_mp(e_=estar, sigma_=par.mpsigma, alpha=par.mpalpha, emin=par.mpemin)
         scale_mp = mp.fmul(scale_mp, a0)
@@ -89,25 +101,36 @@ def main():
             Bnorm = mp.fdiv(cNorm, estar)
             Bnorm = mp.fdiv(Bnorm, estar)
         scale_mp = mp.fdiv(scale_mp, Bnorm)
-        T = mpcov*scale_mp
+        T = mpcov * scale_mp
         T = T + S
-        invT = T**(-1)
+        invT = T ** (-1)
         #   Get coefficients
         gt = h_Et_mp_Eslice(invT, par, estar_=estar)
         rhoE = y_combine_sample_Eslice_mp(gt, mpmatrix=mpcorr_sample, params=par)
         rho[e_i] = rhoE[0]
         drho[e_i] = rhoE[1]
 
-    plt.errorbar(x=espace / par.mpi, y=rho, yerr=drho, marker="o", markersize=1.5, elinewidth=1.3, capsize=2,
-                 ls='', label='HLT (sigma = {:2.2f} Mpi)'.format(par.sigma / par.mpi), color=u.CB_color_cycle[0])
-    plt.xlabel('Energy/Mpi', fontdict=u.timesfont)
-    plt.ylabel('Spectral density', fontdict=u.timesfont)
-    plt.legend(prop={'size': 12, 'family': 'Helvetica'})
+    plt.errorbar(
+        x=espace / par.mpi,
+        y=rho,
+        yerr=drho,
+        marker="o",
+        markersize=1.5,
+        elinewidth=1.3,
+        capsize=2,
+        ls="",
+        label="HLT (sigma = {:2.2f} Mpi)".format(par.sigma / par.mpi),
+        color=u.CB_color_cycle[0],
+    )
+    plt.xlabel("Energy/Mpi", fontdict=u.timesfont)
+    plt.ylabel("Spectral density", fontdict=u.timesfont)
+    plt.legend(prop={"size": 12, "family": "Helvetica"})
     plt.grid()
     plt.tight_layout()
     plt.show()
 
     end()
+
 
 if __name__ == "__main__":
     main()
