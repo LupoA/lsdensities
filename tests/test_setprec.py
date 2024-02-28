1d5399c9999d826d05f810905fb5a212038ce44f
nickforce989 <2227764@swansea.ac.uk>
2024-02-28 05:56:07 +0000

    Issue #42, adding assert to test_setprec.py

--- tests/test_setprec.py
+++ tests/test_setprec.py
@@ -1,9 +1,11 @@
 from lsdensities.utils.rhoUtils import LogMessage
-from mpmath import mp
+from mpmath import mp, mpf
 
 
 def test_init_precision():
     print(LogMessage(), " Initialising...")
-    mp.dps = 64
+    mp.dps = 10
     print(LogMessage(), " Binary precision in bit: ", mp.prec)
     print(LogMessage(), " Approximate decimal precision: ", mp.dps)
+    a = mpf(1 / 3)
+    assert(a - 0.3333333333 < 1e-10)
